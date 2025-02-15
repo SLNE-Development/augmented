ff58089aea243e85810366c9324bca2b9c17755c
Ammo <ammo@slne.dev>
2025-02-15 04:10:36 +0100

    Add bungee messaging api for texting "You received X money for your shop @..." components, etc.

--- augmented-shop/augmented-shop-bukkit/src/main/kotlin/dev/slne/augmented/shop/bukkit/AugmentedShopPlugin.kt
+++ augmented-shop/augmented-shop-bukkit/src/main/kotlin/dev/slne/augmented/shop/bukkit/AugmentedShopPlugin.kt
@@ -2,6 +2,7 @@ package dev.slne.augmented.shop.bukkit
 
 import com.github.shynixn.mccoroutine.folia.entityDispatcher
 import com.github.shynixn.mccoroutine.folia.registerSuspendingEvents
+import com.google.common.io.ByteStreams
 import dev.slne.augmented.common.database.bukkit.plugin.AugmentedDatabasePlugin
 import dev.slne.augmented.shop.api.shopManager
 import dev.slne.augmented.shop.bukkit.commands.ShopCommand
@@ -9,6 +10,7 @@ import dev.slne.augmented.shop.bukkit.listeners.shop.ShopClickListener
 import dev.slne.augmented.shop.bukkit.listeners.shop.ShopDestroyListener
 import dev.slne.augmented.shop.bukkit.listeners.shop.ShopDestroyPreventionListener
 import dev.slne.augmented.shop.bukkit.listeners.shop.ShopPlaceListener
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer
 import org.bukkit.event.Event
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.plugin.java.JavaPlugin
@@ -19,6 +21,8 @@ internal val plugin: AugmentedShopPlugin
 
 class AugmentedShopPlugin : AugmentedDatabasePlugin() {
 
+    lateinit var bungeeChannelApi: BungeeChannelApi
+
     override suspend fun onLoadAsync() {
         super.onLoadAsync()
 
@@ -27,6 +31,8 @@ class AugmentedShopPlugin : AugmentedDatabasePlugin() {
 
     override suspend fun onEnableAsync() {
         super.onEnableAsync()
+
+        bungeeChannelApi = BungeeChannelApi.of(this)
 
         // Commands
         ShopCommand
@@ -37,6 +43,13 @@ class AugmentedShopPlugin : AugmentedDatabasePlugin() {
         pluginManager.registerEvents(ShopDestroyListener, this)
         pluginManager.registerEvents(ShopDestroyPreventionListener, this)
         pluginManager.registerSuspendingEvents(ShopClickListener, this, eventDispatcher)
+
+        bungeeChannelApi.registerForwardListener("SendComponent") { _, player, data ->
+            val input = ByteStreams.newDataInput(data)
+            val component = GsonComponentSerializer.gson().deserialize(input.readUTF())
+
+            player.sendMessage(component)
+        }
     }
 
     override suspend fun onDisableAsync() {
